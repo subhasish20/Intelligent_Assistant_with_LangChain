f330ff3034568d5e1f5398287bac710bf3d7a822
subhasish20 <subhasishjena8280@gmail.com>
2025-09-13 12:27:41 +0530

    updated_file

--- my_ai.py
+++ my_ai.py
@@ -10,6 +10,9 @@ api_key = os.getenv("GOOGLE_API_KEY")
 model = ChatGoogleGenerativeAI(model="gemini-2.5-pro", google_api_key=api_key)
 
 while True:
-    user_inpupt = input("enter your expression :")
-    response = model.invoke(user_inpupt)
-    print(response.content)
+    if user_inpupt == "exit":
+        break
+    else
+        user_inpupt = input("enter your expression :")
+        response = model.invoke(user_inpupt)
+        print(response.content)
